481f731dff41baee4a7f138a4f29571306166fe0
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-12-07 19:12:14 +0700

    Update ocrestart.sh

--- plugins/tools/menu/ocrestart.sh
+++ plugins/tools/menu/ocrestart.sh
@@ -3,7 +3,7 @@
 service openclash restart
 
 # READ AUTH
-if [ -f "/root/TgBotWRT/AUTH" ]; then
+if [ -f "/root/TELEXWRT/AUTH" ]; then
     IFS=$'\n' read -d '' -r -a lines < "/root/TgBotWRT/AUTH"
     if [ "${#lines[@]}" -ge 2 ]; then
         BOT_TOKEN="${lines[0]}"

79c4c12f88b2edf5fc5732122adda90373f56e9b
xodpool <kribonarsis@gmail.com>
2025-03-11 20:04:23 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,20 +2,20 @@
 
 # Asosiasi antara judul dan URL
 declare -A SCRIPTS
-SCRIPTS["system"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/system.sh"
-SCRIPTS["speedtest"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/speedtest.sh"
-SCRIPTS["ping"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/ping.sh"
-SCRIPTS["vnstat"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/vnstat.sh"
-SCRIPTS["restart"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/restart.sh"
-SCRIPTS["reboot"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/reboot.sh"
-SCRIPTS["shutdown"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/shutdown.sh"
-SCRIPTS["ocstart"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/ocstart.sh"
-SCRIPTS["ocrestart"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/ocrestart.sh"
-SCRIPTS["ocstop"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/ocstop.sh"
-SCRIPTS["clear"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/clear.sh"
-SCRIPTS["opkgupdate"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/opkgupdate.sh"
-SCRIPTS["info-modem"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/infoModem.sh"
-SCRIPTS["restart-modem"]="https://raw.githubusercontent.com/ahmadqsyaa/TELEXWRT/main/plugins/tools/menu/restartModem.sh"
+SCRIPTS["system"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/system.sh"
+SCRIPTS["speedtest"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/speedtest.sh"
+SCRIPTS["ping"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/ping.sh"
+SCRIPTS["vnstat"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/vnstat.sh"
+SCRIPTS["restart"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/restart.sh"
+SCRIPTS["reboot"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/reboot.sh"
+SCRIPTS["shutdown"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/shutdown.sh"
+SCRIPTS["ocstart"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/ocstart.sh"
+SCRIPTS["ocrestart"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/ocrestart.sh"
+SCRIPTS["ocstop"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/ocstop.sh"
+SCRIPTS["clear"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/clear.sh"
+SCRIPTS["opkgupdate"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/opkgupdate.sh"
+SCRIPTS["info-modem"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/infoModem.sh"
+SCRIPTS["restart-modem"]="https://raw.githubusercontent.com/xodpool/TELEXWRT/main/plugins/tools/menu/restartModem.sh"
 # Tambahkan judul dan URL lain sesuai kebutuhan
 
 # Memeriksa apakah argumen yang diberikan adalah judul yang valid
